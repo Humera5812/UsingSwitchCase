b3e5b3e9e3d283edf49238e873285dd993cb168c
Humera5812 <humera198.hshs@gmail.com>
2022-07-20 17:17:29 +0530

    Initial Commit

---
+++ UsingSwitchCase/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UsingSwitchCase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UsingSwitchCase.EmpWageUsingSwitch();
+        }
+    }
+}
